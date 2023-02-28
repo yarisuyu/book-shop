910e49c776ae2e264ace9b3621aa1e912e79f38a
Elena Oblomova <lisinaeg@gmail.com>
2023-02-28 12:55:51 +0300

    feat: add popup for book description

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,22 +1,72 @@
-console.log("Here we go!");
 
+let body = document.querySelector("body");
 
-const body = document.querySelector("body");
-
-console.log(body);
+let popupContainer = document.createElement('div');
+popupContainer.className = 'popup';
+popupContainer.id = 'book-descr';
+body.append(popupContainer);
 
 let header = document.createElement('header');
+header.className = "header";
 let main = document.createElement('main');
 let footer = document.createElement('footer');
+footer.className = "footer";
 
 body.append(header);
 body.append(main);
 body.append(footer);
 
-let heading = document.createElement('h1');
-heading.innerText = 'Book Catalog';
-heading.className = 'books__heading';
-main.append(heading);
+const renderDescPopup = function(container, header, author, description) {
+    let descContainer = document.createElement('div');
+    descContainer.className = 'popup__content';
+    container.append(descContainer);
+
+    let descHeader = document.createElement('div');
+    descHeader.className = 'popup__header';
+    descContainer.append(descHeader);
+
+    let descTitle = document.createElement('h2');
+    descTitle.className = 'popup__title';
+    descTitle.innerText = header;
+    descHeader.append(descTitle);
+
+    let close = document.createElement('a');
+    close.href = '#';
+    close.className = 'close';
+    close.innerText = 'x';
+    descHeader.append(close);
+
+    let descAuthor = document.createElement('h3');
+    descAuthor.className = 'popup__author';
+    descAuthor.innerText = author;
+    descContainer.append(descAuthor);
+
+    let descText = document.createElement('p');
+    descText.className = 'popup__description';
+    descText.innerText = description;
+    descContainer.append(descText);
+}
+
+const renderHeader = function () {
+    let wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+    header.append(wrapper);
+
+    let icon = document.createElement('div');
+    icon.className = 'header__ico';
+    icon.innerHTML = 'BookShop';
+    wrapper.append(icon);
+
+    let bag = document.createElement('div');
+    bag.className = 'header__bag';
+
+    let bagIco = document.createElement('img');
+    bagIco.className = 'header__bag-ico';
+    bagIco.setAttribute('src', '../../assets/icons/bag-ico.png');
+    bag.append(bagIco);
+
+    wrapper.append(bag);
+}
 
 const renderBook = function(container, item) {
     let book = document.createElement('div');
@@ -25,9 +75,8 @@ const renderBook = function(container, item) {
     let img = document.createElement('img');
     img.setAttribute('src', item.imageLink);
     img.className = 'books__book-img';
-    img.setAttribute('width', '200');
-    img.setAttribute('height', '260');
-    console.log(img);
+    img.setAttribute('width', '150');
+    img.setAttribute('height', '220');
 
     figure.append(img);
     let figcaption = document.createElement('figcaption');
@@ -48,33 +97,60 @@ const renderBook = function(container, item) {
     bookPrice.className = 'books__book-price';
     figcaption.append(bookPrice);
 
+    let buttons = document.createElement('div');
+    buttons.className = 'books__book-buttons';
+
     let showLink = document.createElement('a');
     showLink.innerText = 'Show more';
+    showLink.href = '#book-descr';
     showLink.className = 'books__book-info-link';
-    figcaption.append(showLink);
+    buttons.append(showLink);
+
+    showLink.addEventListener('click', (event) => {
+        console.log(event);
+        console.log(item.description);
+
+        let popupContainer = document.querySelector('.popup');
+
+        renderDescPopup(popupContainer, item.title, item.author, item.description);
+
+    })
 
     let addBtn = document.createElement('button');
     addBtn.innerText = 'Add to bag';
-    figcaption.append(addBtn);
+    buttons.append(addBtn);
 
+    figcaption.append(buttons);
     figure.append(figcaption);
-    console.log(figure);
-
     book.append(figure);
-    console.log(book);
     container.append(book);
 }
 
-fetch('http://127.0.0.1:5500/assets/data/books.json') //path to the file with json data
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
+const renderMain = function () {
+    let heading = document.createElement('h1');
+    heading.innerText = 'Book Catalog';
+    heading.className = 'books__heading';
+    main.append(heading);
 
-        let bookContainer = document.createElement('div');
-        bookContainer.className = 'books';
-        main.append(bookContainer);
+    fetch('http://127.0.0.1:5500/assets/data/books.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
 
-        data.forEach(book => renderBook(bookContainer, book));
-    });
+            let bookContainer = document.createElement('div');
+            bookContainer.className = 'books';
+            main.append(bookContainer);
+
+            data.forEach(book => renderBook(bookContainer, book));
+        });
+}
+
+const renderFooter = function () {
+
+}
+
+renderHeader();
+renderMain();
+renderFooter();
