d89e5b333fd32b384c45a906621eaeccffbac78a
Elena Oblomova <lisinaeg@gmail.com>
2023-03-20 15:40:07 +0300

    fix: fix shopping cart actions

--- pages/main/script.js
+++ pages/main/script.js
@@ -62,7 +62,7 @@ const renderHeader = function () {
     let icoContainer = addElement(wrapper, 'div');
     addElement(icoContainer, 'a', 'header__ico', 'BookShop', null,
         new Map([
-            ['src', '/']
+            ['href', '../main/']
         ]));
 
     addElement(wrapper, 'div', 'header__motto', 'Books build a stairway to your imagination')
@@ -73,14 +73,13 @@ const renderHeader = function () {
             ['ondragover', 'allowDrop(event)']
         ]));
 
-    //<i class="fa-solid fa-cart-shopping"></i> empty cart
     addElement(bag, 'i', 'header__bag-ico fa-solid fa-cart-shopping');
 
     addElement(bag, 'span', 'header__bag-sum', ' 0$ ', 'header-cart');
-    addElement(bag, 'i', 'header__bag-show-ico fa-solid fa-angle-down', null, null,
-        new Map([
-            ['onclick', 'showCart(event)']
-        ]));
+    let toggleCartIco = addElement(bag, 'i', 'header__bag-show-ico fa-solid fa-angle-down', null, 'cart-toggler');
+    toggleCartIco.addEventListener('click', toggleCartOpen);
+
+    updateCartSum();
 }
 
 const renderBook = function (container, id, item) {
@@ -105,14 +104,20 @@ const renderBook = function (container, id, item) {
     let buttons = addElement(figcaption, 'div', 'books__book-buttons');
 
     let showLink = addElement(buttons, 'a', 'books__book-info-link', 'Show more', null,
-        new Map([['href', '#book-descr']]));
+        new Map([
+            ['href', '#book-descr'],
+            ['title', 'Show book description']
+        ]));
 
     showLink.addEventListener('click', (event) => {
         let popupContainer = document.querySelector('.popup');
         renderDescPopup(popupContainer, item.title, item.author, item.description);
     })
 
-    let addBtn = addElement(buttons, 'button', null, 'Add to bag');
+    let addBtn = addElement(buttons, 'button', null, 'Add to bag', null,
+        new Map([
+            ['title', 'Add to bag']
+        ]));
 
     addBtn.addEventListener('click', (event) => {
         addToCart({ ...item, id });
@@ -146,30 +151,199 @@ renderHeader();
 renderMain();
 renderFooter();
 
-function updateCartSum(price) {
-    let sum = +localStorage.getItem('shoppingCartSum');
-    sum += price;
-    localStorage.setItem('shoppingCartSum', sum);
 
-    let headerCart = document.getElementById('header-cart');
-    headerCart.innerText = `Total ${sum}$`;
+
+function renderCartItem(bookItem, value) {
+    addElement(bookItem, 'div', 'cart__book-title', `${value.title}`);
+    addElement(bookItem, 'div', 'cart__book-author', `${value.author}`);
+    addElement(bookItem, 'div', 'cart__book-price', `${value.price}$`);
+
+    const bookCount = addElement(bookItem, 'div', 'cart__book-count');
+    const decBtn = addElement(bookCount, 'button', 'cart__book-count-btn', '−', null,
+        new Map([
+            ['title', 'Remove one']
+        ]));
+    addElement(bookCount, 'span', 'cart__book-count-value', value.count);
+    const incBtn = addElement(bookCount, 'button', 'cart__book-count-btn', '+', null,
+        new Map([
+            ['title', 'Add one more']
+        ]));
+    if (value.count == 1) {
+        decBtn.disabled = true;
+    }
+
+    const removeIco = addElement(bookItem, 'i', 'cart__book-remove fa-solid fa-xmark', null, null,
+        new Map([
+            ['title', 'Remove from the cart']
+        ]));
+
+    decBtn.addEventListener('click', (event) => decBookCountInCart(event));
+    incBtn.addEventListener('click', (event) => incBookCountInCart(event));
+    removeIco.addEventListener('click', (event) => removeBookFromCart(event));
 }
 
+function renderCart(outerContainer) {
+    let container = addElement(outerContainer, 'div', 'cart', null, 'cart-popup');
 
+    let wrapper = addElement(container, 'div', 'cart__wrapper', null, 'cart-wrapper');
+    addElement(wrapper, 'h2', 'cart__header', 'Shopping cart');
+
+    let booksEncrypted = localStorage.getItem('shoppingCart');
+    if (booksEncrypted && booksEncrypted.length > '{}'.length)
+    {
+        let bookContainer = addElement(wrapper, 'div', 'cart__books', null, 'cart-books');
+        let fragment = new DocumentFragment();
+
+        let books = new Map(Object.entries(JSON.parse(booksEncrypted) ?? []));
+
+        let sum = 0;
+        books.forEach((book, id) => {
+            sum += book.price * book.count;
+            let bookItem = document.createElement('div');
+            bookItem.id = `${id}-cart-book`;
+            bookItem.className = 'cart__book';
+
+            renderCartItem(bookItem, book);
+            fragment.appendChild(bookItem);
+        });
+
+        bookContainer.appendChild(fragment);
+
+        const total = addElement(wrapper, 'div', 'cart__total-sum', null, 'cart-total');
+        total.innerHTML = `<b>Total:</b> ${sum}$`;
+
+        const buttons = addElement(wrapper, 'div', 'cart__buttons')
+        addElement(buttons, 'button', 'cart__clear-btn', 'Clear', null,
+            new Map([
+                ['onclick', 'clearCart()'],
+                ['title', 'Clear cart']
+            ]));
+        addElement(buttons, 'a', 'cart__confirm-btn small', 'Confirm order', null, new Map([
+            ['href', '../order-wrapper'],
+            ['title', 'Confirm order']
+        ]));
+    } else {
+        addElement(wrapper, 'div', null, 'Cart is empty');
+    }
+}
+
+function refreshCart() {
+    if (!isCartOpen()) return;
+
+    const cartPopup = document.getElementById('cart-popup');
+
+    let cart = localStorage.getItem('shoppingCart');
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    const cartBookContainer = document.getElementById('cart-books');
+    if (!cartBookContainer) {
+        toggleCartOpen();
+        return;
+    }
+
+    cartBookContainer.childNodes.forEach(book => {
+        const bookId = `${parseInt(book.id)}`;
+        if (!shoppingCart.get(bookId)) {
+            cartBookContainer.removeChild(book);
+        }
+    });
+
+    let cartSum = 0;
+    // for books present in storage
+    shoppingCart.forEach((book, id) => {
+        cartSum += parseFloat(book.price) * parseInt(book.count);
+
+        // add to book list if not in cart yet
+        let bookItem = document.getElementById(`${id}-cart-book`);
+        if (!bookItem) {
+            bookItem = document.createElement('div');
+            bookItem.id = `${id}-cart-book`;
+            bookItem.className = 'cart__book';
+
+            renderCartItem(bookItem, book);
+
+            cartBookContainer.appendChild(bookItem);
+        } else {
+            // update book count if book is in cart
+            bookItem.querySelector('.cart__book-count-value').innerHTML = book.count;
+        }
+    });
+
+    // update total count
+    let totalSum = document.getElementById('cart-total');
+    totalSum.innerHTML = `<b>Total:</b> ${cartSum}$`;
+}
+
+/* Start of cart handling functions */
+
+function addBookToStorage(id, book) {
+    let cart = localStorage.getItem('shoppingCart');
+    // cart is stored as a stringified Map of id : {...book, count}
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    let bookInCart = shoppingCart.get(id);
+
+    if (bookInCart) {
+        bookInCart.count++;
+    }
+    else {
+        bookInCart = book;
+    }
+
+    shoppingCart.set(id, bookInCart);
+    localStorage.setItem('shoppingCart', JSON.stringify(Object.fromEntries(shoppingCart)));
+
+}
+
+function isCartOpen() {
+    let cartPopup = document.getElementById('cart-popup');
+    return cartPopup !== null;
+}
+
+function toggleCartAngleIco() {
+    let toggleCartIco = document.getElementById('cart-toggler');
+    toggleCartIco.classList.toggle("fa-angle-down");
+    toggleCartIco.classList.toggle("fa-angle-up");
+}
+
+function toggleCartOpen() {
+    toggleCartAngleIco();
+    let toggleCartIco = document.getElementById('cart-toggler');
+    if (toggleCartIco && toggleCartIco.classList.contains("fa-angle-up")) {
+        renderCart(body);
+    } else if (isCartOpen()) {
+        const cartPopup = document.getElementById('cart-popup');
+        body.removeChild(cartPopup);
+    }
+}
+
+function updateCartSum() {
+    let cart = localStorage.getItem('shoppingCart');
+
+    // cart is stored as a stringified Map of id : {...book, count}
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    let sum = 0;
+    shoppingCart.forEach(book => {
+        sum += parseFloat(book.price) * parseInt(book.count);
+    });
+
+    // update total sum in header
+    let headerCart = document.getElementById('header-cart');
+    headerCart.innerHTML = `Total ${sum}$`;
+
+    return sum;
+}
+
+/* Add a book to cart by drag and drop */
 function drag(ev) {
-    console.log('Drag: ');
-    console.log(ev);
-
     let element = ev.target;
-
-    console.log(element.parentElement.parentElement.id);
     let sibling = element.nextSibling;
     let author = sibling.querySelector('.books__book-author').innerText;
     let title = sibling.querySelector('.books__book-title').innerText;
-    let price = sibling.querySelector('.books__book-price').innerText;
+    let price = parseFloat(sibling.querySelector('.books__book-price').innerText);
 
-    const data = { id, title, author, price }
-    console.log(data);
+    const data = { id: element.id, title, author, price };
 
     ev.dataTransfer.setData('text', JSON.stringify(data));
 }
@@ -181,102 +355,118 @@ function allowDrop(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData('text');
-    console.log('Dropped: ');
-
     var parsedData = JSON.parse(data);
-    console.log(parsedData);
 
-    let cart = localStorage.getItem('shoppingCart');
-    console.log(cart);
-    // cart is stored as a stringified Map of id : {...book, count}
-    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
-    console.log(shoppingCart);
-
-    // if cart contains this book, increment count
     let id = parsedData.id;
-    let book = shoppingCart[id];
-    if (book) {
-        book.count++;
-    }
-    else {
-        book = {
-            title: parsedData.title,
-            author: parsedData.author,
-            price: parsedData.price,
-            count: 1
-        };
-    }
+    let book = {
+        title: parsedData.title,
+        author: parsedData.author,
+        price: parsedData.price,
+        count: 1
+    };
 
-    shoppingCart.set(id, book);
-    console.log(JSON.stringify(Object.fromEntries(shoppingCart)));
-    localStorage.setItem('shoppingCart', JSON.stringify(Object.fromEntries(shoppingCart)));
+    addBookToStorage(id, book);
+    updateCartSum();
 
-    updateCartSum(+book.price);
+    refreshCart();
 }
 
+
+/* Add a book to cart by button click */
 function addToCart(data) {
-    console.log(data);
-    let cart = localStorage.getItem('shoppingCart');
-    console.log(cart);
-
-    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
-    console.log(shoppingCart);
-
     let id = `${data.id}`;
-    let book = shoppingCart.get(id);
-    if (book) {
-        book.count = book.count + 1;
-        console.log(book.count);
-    }
-    else {
-        book = {
+    let book = {
             title: data.title,
             author: data.author,
             price: data.price,
             count: 1
         };
-    }
-    console.log(book);
-    shoppingCart.set(id, book);
-    console.log(shoppingCart);
-    console.log(Object.fromEntries(shoppingCart));
 
-    console.log(JSON.stringify(Object.fromEntries(shoppingCart)));
+
+    addBookToStorage(id, book);
+
+    updateCartSum();
+
+    refreshCart();
+}
+
+
+function removeBookFromCart(event) {
+    const bookId = `${parseInt(event.target.parentElement.id)}`;
+
+    let cart = localStorage.getItem('shoppingCart');
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    shoppingCart.delete(bookId);
     localStorage.setItem('shoppingCart', JSON.stringify(Object.fromEntries(shoppingCart)));
 
-    updateCartSum(+book.price);
+    updateCartSum();
 
+    // if it was the last book, close cart
+    if (shoppingCart.size === 0) {
+        toggleCartOpen();
+    } else {
+        refreshCart();
+    }
 }
 
-function openCart() {
-    document.getElementById("cart-popup").style.display = "block";
+function decBookCountInCart(event) {
+    const bookId = `${parseInt(event.target.parentElement.parentElement.id)}`;
+
+    let cart = localStorage.getItem('shoppingCart');
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    let bookInCart = shoppingCart.get(bookId);
+
+    if (bookInCart) {
+        bookInCart.count--;
+    }
+    else {
+        return;
+    }
+
+    if (bookInCart.count > 0) {
+        shoppingCart.set(bookId, bookInCart);
+    } else {
+        shoppingCart.delete(bookId);
+    }
+
+    localStorage.setItem('shoppingCart', JSON.stringify(Object.fromEntries(shoppingCart)));
+
+    updateCartSum();
+
+    refreshCart();
 }
 
-function closeCart() {
-    document.getElementById("cart-popup").style.display = "none";
+function incBookCountInCart(event) {
+    const bookId = `${parseInt(event.target.parentElement.parentElement.id)}`;
+
+    let cart = localStorage.getItem('shoppingCart');
+    let shoppingCart = new Map(Object.entries(JSON.parse(cart) ?? []));
+
+    let bookInCart = shoppingCart.get(bookId);
+
+    if (bookInCart) {
+        bookInCart.count++;
+    }
+    else {
+        return;
+    }
+
+    shoppingCart.set(bookId, bookInCart);
+
+    localStorage.setItem('shoppingCart', JSON.stringify(Object.fromEntries(shoppingCart)));
+
+    updateCartSum();
+
+    refreshCart();
 }
 
-function renderCart(outerContainer) {
-    let container = addElement(outerContainer, 'div', 'cart', null, 'cart-popup');
+function clearCart() {
+    localStorage.removeItem('shoppingCart');
+    updateCartSum();
 
-    let form = addElement(container, 'form', 'cart__form');
-    let header = addElement(form, 'h2', 'cart__header', 'Shopping cart');
-    let bookContainer = addElement(form, 'div', 'cart__books');
-
-    let booksEncrypted = localStorage.getItem('shoppingCart');
-    let books = new Map(Object.entries(JSON.parse(booksEncrypted) ?? []));
-
-    Object.values(books).forEach(value => {
-        addElement(bookContainer, 'div', 'cart__book-title', `${value.title}`);
-        addElement(bookContainer, 'div', 'cart__book-author',`${value.author}`);
-        addElement(bookContainer, 'div', 'cart__book-price', `${value.price}`);
-        addElement(bookContainer, 'a', 'cart__book-close','x', null, new Map(['onclick', 'onRemove(ev)']));
-    })
-
-    let totalSum = localStorage.getItem('shoppingCartSum');
-
-    addElement(form, 'div', 'cart__total-sum', totalSum + '$' );
-    addElement(form, 'button', 'cart__confirm-btn', 'Confirm order', null, new Map(['onclick', 'onCheckout()']))
-    addElement(form, 'button', 'cart__close-btn', 'Close', null, new Map(['onclick', 'closeCart()']))
-
+    toggleCartOpen();
 }
+
+/* End of cart handling functions */
